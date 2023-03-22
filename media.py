8f92947bee9bf45df96bd194e4d57ef533a41a27
KassidyPrice <96833138+KassidyPrice@users.noreply.github.com>
2023-03-21 22:32:39 -0600

    fixed tags, added img src

--- media.py
+++ media.py
@@ -59,36 +59,27 @@ for card in asset_cards:
     content_soup = asset_soup.find('div', class_='sc-9dd4b4e5-1 jBFjPW') 
 
     description = ''
+    image_src = ''
     if content_soup:
         description = content_soup.find('div', class_ = 'sc-1szn76m-0 kFzbNE').text.strip()
-        scripture_ref_soup = content_soup.find('div', class_ = 'sc-1u44evz-0 JDVKk')
-        print(scripture_ref_soup)
-        # refs_array = []
-        # if scripture_ref_soup:
-        #     scripture_refs = scripture_ref_soup.find('span')
-            
-            
-            # for ref in scripture_refs:
-            #     ref = ref.text.strip()
-                # refs_array.append(ref)
+        image = content_soup.find('img', class_ = 'sc-voqve2-0 ktWpyD')
+        image_src = image['src']
+        tags_soup = content_soup.find_all('span', class_ = 'sc-kb0k5s-0 gSUFTs')
+        tags_array = []
+        if tags_soup:
+            for tag in tags_soup:
+                tag = tag.text.strip()
+                tags_array.append(tag)
+    
 
-   
-    # tags_soup = asset_soup.find('span', class_ = 'sc-kb0k5s-0 gSUFTs')
-    # tags_array = []
-    # if tags_soup:
-    #     tags = tags_soup.find_all('a')
-    #     for tag in tags:
-    #         tag = tag.text.strip()
-    #         tags_array.append(tag)
-    # print(tags_array)
+    if title:
+        next_asset = {
+            'asset_title':title,
+            'description': description,
+            'image_src':image_src,
+            'tags': tags_array,
+            'asset_link':f'https://www.churchofjesuschrist.org/{link_href}'
+        }
+        asset_data.append(next_asset)
 
-#     next_asset = {
-#         'asset_title':title,
-#         'description': description,
-#         'scripture_ref': ref,
-#         'tags': tags_array,
-#         'asset_link':f'https://www.churchofjesuschrist.org/{link_href}'
-#     }
-#     asset_data.append(next_asset)
-
-# pprint.pprint(asset_data)
+pprint.pprint(asset_data)
